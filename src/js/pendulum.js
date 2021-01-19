89762d9c3705f6788cf39a4b56f57354c86102ed
SpyGuy <shashankprasanna1@gmail.com>
2021-01-19 17:52:06 -0500

    stuff

---
+++ src/js/pendulum.js
@@ -0,0 +1,25 @@
+class Ball {
+	constructor(x, y) {
+		var options = {
+			isStatic: false,
+			restitution: 1.2,
+			friction: 0.4,
+			density: 1,
+		};
+		this.radius = 32;
+		this.body = Bodies.circle(x, y, this.radius, options);
+		this.x = x;
+		this.y = y;
+
+		World.add(world, this.body);
+	}
+
+	display() {
+		push();
+		translate(this.body.position.x, this.body.position.y);
+		ellipseMode(CENTER);
+		fill(0, 0, 0, 0);
+		ellipse(0, 0, this.radius * 2, this.radius * 2);
+		pop();
+	}
+}
